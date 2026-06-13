0e93074a8940e0126f779bd2f704bc83e0b5b99b
TopMyster <topmyster@outlook.com>
2026-06-13 16:18:39 -0500

    feat: Adds Socials

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -5,22 +5,26 @@ export default function MainContent() {
             <h2 style={{fontWeight: 500, margin: 0, marginBottom: 8, opacity: 1}}>Toope Oladunjoye</h2>
             <h3 style={{fontWeight: 100, margin: 0, marginBottom: 30, opacity: .7}}>Software Engineer</h3>
             <div className="sections">
-                <Section name="About" isLink={false} content={[{title: "Hi, I'm Toope! I enjoy coding, designing software, and playing video games.", link: ""}]}/>
-                <Section name="Work" isLink={true} content={[
-                    {title: "Pokemon BSP", link: "https://github.com/TopMyster/pokemon-python"},
-                    {title: "DeskTama", link: "https://github.com/TopMyster/DeskTama"},
-                    {title: "Rayterm", link: "https://github.com/TopMyster/rayterm"},
-                    {title: "Forma", link: "https://formasite.vercel.app/"},
-                    {title: "Nova", link: "https://github.com/TopMyster/Nova"}
+                <Section name="About" isLink={false} isImage={false} content={[{title: "Hi, I'm Toope! I enjoy coding, designing software, and playing video games.", link: "", image: "", size: 0}]}/>
+                <Section name="Work" isLink={true} isImage={false} content={[
+                    {title: "Pokemon BSP", link: "https://github.com/TopMyster/pokemon-python", image: "", size: 0},
+                    {title: "DeskTama", link: "https://github.com/TopMyster/DeskTama", image: "", size: 0},
+                    {title: "Rayterm", link: "https://github.com/TopMyster/rayterm", image: "", size: 0},
+                    {title: "Forma", link: "https://formasite.vercel.app/", image: "", size: 0},
+                    {title: "Nova", link: "https://github.com/TopMyster/Nova", image: "", size: 0}
                 ]}/>
-                <Section name="Files" isLink={true} content={[
-                    {title: "Resumé", link: "./src/assets/Files/Resume.pdf"},
-                    {title: "Curriculum Vitae", link: "./src/assets/Files/CV.pdf"}
+                <Section name="Files" isLink={true} isImage={false} content={[
+                    {title: "Resumé", link: "./src/assets/Files/Resume.pdf", image: "", size: 0},
+                    {title: "Curriculum Vitae", link: "./src/assets/Files/CV.pdf", image: "", size: 0}
                 ]}/>
-                <Section name="Papers" isLink={true} content={[
-                    {title: "BCNY's Purpose Shift: Arc to Dia", link: "./src/assets/Papers/BCNY's Purpose Shift: Arc to Dia.pdf"}
+                <Section name="Papers" isLink={true} isImage={false} content={[
+                    {title: "BCNY's Purpose Shift: Arc to Dia", link: "./src/assets/Papers/BCNY's Purpose Shift: Arc to Dia.pdf", image: "", size: 4}
                 ]}/>
-                <Section name="Socials"/>
+                <Section name="Socials" isLink={true} isImage={true} content={[
+                    {title: "BlueSky", link: "https://bsky.app", image: "./src/assets/Socials/Bluesky.png", size: 23},
+                    {title: "Threads", link: "https://threads.com", image: "./src/assets/Socials/Threads.png", size: 20},
+                    {title: "Linkedin", link: "https://linkedin.com", image: "./src/assets/Socials/Linkedin.png", size: 23}
+                ]}/>
             </div>
         </>
     )
--- src/Section.tsx
+++ src/Section.tsx
@@ -3,32 +3,40 @@ import { useState } from "react";
 interface ContentItem {
     title: string
     link: string
+    image: string
+    size: number
 }
 interface SectionProps {
     name: string;
     content?: ContentItem[];
     isLink?: boolean;
+    isImage?: boolean
 }
 
-export default function Section({ name, content, isLink = false }: SectionProps) {
+export default function Section({ name, content, isLink = false, isImage = false }: SectionProps) {
     const [isOpen, setIsOpen] = useState(false)
+    const [isHovered, setIsHovered] = useState(false)
     return (
         <>
             <ul style={{padding: 0, margin: 0}}>
                 <li style={{listStyle: "none", marginBottom: 15}}>
                     <div
-                        style={{fontSize: 20, textDecoration: "none", cursor: "pointer", display: "inline-block", fontWeight: isOpen ? 500 : undefined, opacity: isOpen ? 1 : undefined}}
+                        style={{fontSize: 20, textDecoration: "none", cursor: "pointer", display: "inline-block", fontWeight: isOpen ? 500 : undefined, opacity: isOpen || isHovered ? 1 : 0.7}}
                         onClick={() => setIsOpen(!isOpen)}
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
                     >
                         {name}
                     </div>
-                    {content && isOpen && (
+                    <div style={{ display: "flex", flexDirection: isImage ? "row" : "column", marginLeft: isImage ? 5 : 0}}>
+                        {content && isOpen && (
                         content.map((item, index) => (
-                            <div key={index} style={{marginTop: 15, marginLeft: 15, fontSize: 17}}>
-                            {isLink ? <a href={item.link} target="_blank" style={{textDecoration: "underline", color: "inherit"}}>{item.title}↗</a> : <div>{item.title}</div>}
-                        </div>
-                        ))
-                    )}
+                            <div key={index} className={isImage ? "image" : "item"} style={{marginTop: 15, marginLeft: isImage ? 8 : 15, fontSize: 17}}>
+                                {isLink && !isImage? <a href={item.link} target="_blank" style={{textDecoration: "underline", color: "inherit"}}>{item.title}↗</a> : isImage && isLink ? <a href={item.link} target="_blank"><img className={name} src={item.image} width={item.size} style={{margin: 0}}/></a> : <div>{item.title}</div>}
+                            </div>
+                            ))
+                        )}
+                    </div>
                 </li>
             </ul>
         </>
