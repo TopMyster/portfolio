2d6da361bef12dfa59627e09a935d85af04ea910
TopMyster <topmyster@outlook.com>
2026-06-15 14:09:53 -0500

    feat: Easter Egg

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,17 +1,63 @@
+import { useEffect, useState } from "react"
 import MusicPlayer from "./MusicPlayer"
 import Section from "./Section"
+import { Toaster, toast } from "sonner"
+
 export default function MainContent() {
+    const [isEasterEgg, setIsEasterEgg] = useState(false)
+    const [score, setScore] = useState(0)
+    const [done, setDone] = useState(false)
+
+    useEffect(() => {
+        if (score == 11) {
+            toast(
+                <>
+                    Reward:<br></br>
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" style={{ color: "grey" }} target="_blank">Here's this link</a>
+                </>
+            )
+        } else if (score == 51) {
+            toast(
+                <>
+                    Reward:<br></br>
+                    <a href="https://www.google.com/search?q=persona%204" style={{ color: "grey" }} target="_blank">Here's my favorite Game. Try it out! </a>
+                </>
+            )
+        } else if (score == 101) {
+            toast(
+                <div style={{lineHeight: 2}}>
+                    Thanks for checking out my portfolio!<br></br>
+                    <a style={{ color: "grey" }}>Here's another song for you to listen to</a><br></br>
+                    <iframe data-testid="embed-iframe" style={{borderRadius: 12}} src="https://open.spotify.com/embed/track/76YdWrd7Yw9GuU0LirE088?utm_source=generator&si=40a4b732c6c14488" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </div>
+            )
+            setDone(true)
+        }
+    })
+
     return (
         <>
-            <MusicPlayer/>
-            <h2 style={{fontWeight: 500, margin: 0, marginBottom: 8, opacity: 1, cursor: "default" }}>Toope Oladunjoye</h2>
+            {isEasterEgg ? 
+                <Toaster 
+                    duration={done ? Infinity : undefined}
+                    closeButton={done}
+                />
+            : null}
+            <MusicPlayer/> 
+            <h2 
+                style={{fontWeight: 500, margin: 0, marginBottom: 8, opacity: 1, cursor: "pointer", userSelect: isEasterEgg ? "none" : undefined, WebkitUserSelect: isEasterEgg ? "none" : undefined, MozUserSelect: isEasterEgg ? "none" : undefined }}
+                onClick={() => {setIsEasterEgg(true); score <= 100 ? setScore(score+1) : null; score <= 100 ? toast(`Your Score is: ${score}`) : null}}
+            >
+                Toope Oladunjoye
+            </h2>
             <h3 style={{fontWeight: 100, margin: 0, marginBottom: 30, opacity: .7, cursor: "default" }}>Software Engineer</h3>
             <div className="sections">
                 <Section 
                     name="About" 
                     isLink={false} 
                     isImage={false} 
-                    content={[{title: "Hi, I'm Toope! I enjoy coding, designing software, and playing video games.", 
+                    content={[
+                        {title: "Hi, I'm Toope! I enjoy coding, designing software, and playing video games.", 
                         link: "", 
                         image: "", 
                         width: 0, 
