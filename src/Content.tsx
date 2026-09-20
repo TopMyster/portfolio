d13b5cc56bad7d32040ff986dc1e14700d6a6c73
TopMyster <topmyster@outlook.com>
2026-09-20 18:30:55 -0500

    fix: Increases gap between sections and new easter egg

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import MusicPlayer from "./MusicPlayer"
 import Section from "./Section"
-import { Toaster, toast } from "sonner"
 import Scritto from "@scritto/react"
 
 export default function Content() {
-    const [isEasterEgg, setIsEasterEgg] = useState(false)
-    const [score, setScore] = useState(0)
-    const [done, setDone] = useState(false)
     const [count, setCount] = useState(0)
+    const [nameCount, setNameCount] = useState(0)
 
     const descs: Record<number, string> = {
         1 : "Software Engineer",
@@ -20,61 +17,18 @@ export default function Content() {
 
     const desc = descs[count] || "Software Engineer"
 
-    useEffect(() => {
-        if (!isEasterEgg) return
-
-        toast(`Your Score is: ${score}`);
-        
-        if (score === 10) {
-            toast(
-                <>
-                    Reward:<br></br>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs" style={{ color: "grey" }} target="_blank">Here's this link</a>
-                </>
-            )
-        } else if (score === 50) {
-            toast(
-                <>
-                    Reward:<br></br>
-                    <a href="https://www.google.com/search?q=persona%204" style={{ color: "grey" }} target="_blank">Here's my favorite Game. Try it out! </a>
-                </>
-            )
-        } else if (score === 100) {
-            toast(
-                <div style={{lineHeight: 2}}>
-                    Thanks for checking out my portfolio!<br></br>
-                    <a style={{ color: "grey" }}>Here's another song for you to listen to</a><br></br>
-                    <iframe data-testid="embed-iframe" style={{borderRadius: 12}} src="https://open.spotify.com/embed/track/76YdWrd7Yw9GuU0LirE088?utm_source=generator&si=40a4b732c6c14488" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                </div>
-            )
-            setDone(true)
-        }
-    }, [score, isEasterEgg])
+    const name = nameCount === 0 ? "Toope Oladunjoye" : "That's my name :)"
 
     return (
         <>
-            {isEasterEgg ? 
-                <Toaster 
-                    duration={done ? Infinity : undefined}
-                    closeButton={done}
-                />
-            : null}
             <MusicPlayer/> 
-            <h2 
-                style={{fontWeight: 500, margin: 0, marginBottom: 8, opacity: 1, cursor: "pointer", userSelect: isEasterEgg ? "none" : undefined, WebkitUserSelect: isEasterEgg ? "none" : undefined, MozUserSelect: isEasterEgg ? "none" : undefined }}
-                onClick={() => {
-                    setIsEasterEgg(true);
-                    if (score < 100) {
-                        let newScore = score + 1
-                        setScore(newScore);
-                        const audio = new Audio('/assets/music/sound-effect.mp3')
-                        audio.play()
-                    }
-                }}
-            >
-                Toope Oladunjoye
-            </h2>
-            <Scritto onClick={() => {setCount(count < 5 ? count + 1  : 1)}} value={desc} trend={1} transition={{ duration: 400 }} style={{ fontSize: 19, fontWeight: 100, margin: 0, marginBottom: 30, opacity: .7, cursor: "default" }}/>
+            <Scritto onClick={() => {
+                setNameCount(1)
+                setTimeout(() => {
+                    setNameCount(0)
+                }, 1000)
+            }} value={name} trend={1} transition={{ duration: 500 }} style={{ fontSize: 23, fontWeight: 500, margin: 0, marginBottom: 8, opacity: 1, cursor: "pointer", userSelect: "none", WebkitUserSelect: "none", MozUserSelect: "none"}}/>
+            <Scritto onClick={() => {setCount(count < 5 ? count + 1  : 1)}} value={desc} trend={1} transition={{ duration: 400 }} style={{ fontSize: 19, fontWeight: 100, margin: 0, marginBottom: 40, opacity: .7, cursor: "default" }}/>
               <div className="sections">
                 <Section 
                     name="About" 
